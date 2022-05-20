2d24e81bcf6d0d15b004c61105332339eccfa6db
Chris Sims <chris@jcsi.ms>
2022-05-19 21:14:46 -0700

    home.nix cleanup, remove a ton of gpg options

--- home.nix
+++ home.nix
@@ -58,7 +58,9 @@ rec {
                                      tg-update-client
                                      desync]
         else [])
-  ++ (if pkgs.stdenv.isDarwin then [iterm2 pinentry_mac terminal-notifier]
+  ++ (if pkgs.stdenv.isDarwin then [iterm2
+                                    pinentry_mac
+                                    terminal-notifier]
       else []);
 
   programs.git = {
@@ -100,24 +102,8 @@ rec {
     enable = true;
     settings = {
       default-key = "0x25FF041622DE3AFB";
-      personal-cipher-preferences = "AES256 AES192 AES";
-      personal-digest-preferences = "SHA512 SHA384 SHA256";
-      personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
-      default-preference-list = "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
-      cert-digest-algo = "SHA512";
-      s2k-digest-algo = "SHA512";
-      s2k-cipher-algo = "AES256";
       charset = "utf-8";
-      fixed-list-mode = true;
-      no-comments = true;
-      no-emit-version = true;
-      no-greeting = true;
       keyid-format = "0xlong";
-      list-options = "show-uid-validity";
-      verify-options = "show-uid-validity";
-      with-fingerprint = true;
-      require-cross-certification = true;
-      no-symkey-cache = true;
       use-agent = true;
     };
   };
@@ -138,8 +124,8 @@ rec {
                       "$HOME/go/bin"
                       "$HOME/.cargo/bin"] ++
   (if pkgs.stdenv.isDarwin then
-                        ["/opt/homebrew/bin"
-                         "/opt/homebrew/sbin"]
+    ["/opt/homebrew/bin"
+     "/opt/homebrew/sbin"]
    else []);
 
   home.sessionVariables = {
