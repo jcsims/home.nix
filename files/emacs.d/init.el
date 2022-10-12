3f59d97f7c1c2442c88b949068a25fb667ebf12e
Chris Sims <chris@jcsi.ms>
2022-10-11 22:00:14 -0700

    Handle no meetings in a day

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -1060,17 +1060,20 @@ Passes ARG onto `zap-to-char` or `backward-kill-word` if used."
   (obsidian-specify-path "~/notes/work")
   (global-obsidian-mode)
 
+  (require 'seq)
   (defun jcs/open-todays-meeting ()
     "Open an Obsidian meeting note from today."
     (interactive)
     (let* ((today-string (format-time-string "%Y-%m-%d"))
 	   (meeting-dir (expand-file-name "meetings" obsidian-directory))
-	   (choices (->> (directory-files-recursively meeting-dir "\.*$")
-			 (-filter #'obsidian-file-p)
-			 (-map (lambda (f) (file-relative-name f meeting-dir)))
-			 (-filter (lambda (f) (s-starts-with? today-string f)))))
-	   (choice (completing-read "Select file: " choices)))
-      (obsidian-find-file (expand-file-name choice meeting-dir))))))
+	   (choices (->> (directory-files-recursively meeting-dir "\.*.md$")
+			 (seq-filter #'obsidian-file-p)
+			 (seq-map (lambda (f) (file-relative-name f meeting-dir)))
+			 (seq-filter (lambda (f) (s-starts-with? today-string f))))))
+      (if choices
+	  (obsidian-find-file (expand-file-name (completing-read "Select file: " choices)
+						meeting-dir))
+	(message "No meeting files for today.")))))
 
 ;; Local personalization
 (let ((file (expand-file-name (concat (user-real-login-name) ".el")
