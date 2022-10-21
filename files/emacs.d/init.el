d22153b6628acaf51cf86e1358f54c195e297654
Chris Sims <chris@jcsi.ms>
2022-10-21 16:00:54 -0700

    Add org config back

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -138,10 +138,328 @@
 (use-package windmove
   :config (windmove-default-keybindings '(super meta)))
 
+(use-package vulpea)
+
+;; Quick access to a few files
+(defvar org-dir "~/org/")
+(defvar jcs/org-roam-dir (file-truename "~/org-roam"))
+
+(use-package org
+  :custom
+  (org-priority-default ?C)
+  (org-priority-lowest ?D)
+  (org-tag-alist (quote (("@alex" . ?a)
+			 ("@stacey" . ?s)
+			 ("@uma" . ?u)
+			 ("@ed" . ?e)
+			 ("@tega" . ?t)
+			 (:newline)
+			 ("important" . ?i)
+			 ("urgent" . ?r)
+			 ("to_delegate" . ?d))))
+
+  :config
+  (setq org-hide-leading-stars t
+	org-hide-emphasis-markers t ;; Hide things like `*` for bold, etc.
+	org-directory org-dir
+	org-log-done 'time
+	org-log-into-drawer t
+	org-startup-indented t
+	org-startup-folded t
+	org-src-fontify-natively t
+	org-use-fast-todo-selection t
+	org-outline-path-complete-in-steps nil
+	;; Don't ask every time before evaluating an org source block
+	org-confirm-babel-evaluate nil
+	;; Display images in org by default
+	org-startup-with-inline-images t
+	;; Try to keep image widths in emacs to a sane value (measured in pixels)
+	org-image-actual-width 1000)
+  (setq org-todo-keywords
+	(quote ((sequence "TODO(t)" "DOING(o)" "|" "DONE(d)")
+		(sequence "DELEGATED(e@/!)" "WAITING(w@/!)" "BLOCKED(b@/!)" "HAMMOCK(h@/!)" "|" "CANCELLED(c@/!)"))))
+
+  ;; These tend to modify files, so save after doing it
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+  (advice-add 'org-archive-subtree-default :after 'org-save-all-org-buffers)
+  (advice-add 'org-agenda-archive-default-with-confirmation :after 'org-save-all-org-buffers)
+  (advice-add 'org-agenda-todo :after 'org-save-all-org-buffers)
+
+  ;; Modified from https://stackoverflow.com/a/31868530
+  (defun jcs/org-screenshot ()
+    "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+    (interactive)
+    (setq filename
+          (concat
+           (make-temp-name
+            (concat (file-name-nondirectory (buffer-file-name))
+                    "_imgs/"
+                    (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+    (unless (file-exists-p (file-name-directory filename))
+      (make-directory (file-name-directory filename)))
+					; take screenshot
+    (if (eq system-type 'darwin)
+	(call-process "screencapture" nil nil nil "-i" filename))
+    (if (eq system-type 'gnu/linux)
+	(call-process "import" nil nil nil filename))
+    ;; insert into file if correctly taken
+    (if (file-exists-p filename)
+	(insert (concat "[[file:" filename "]]")))
+    (org-display-inline-images))
+
+  ;; Borrowed from http://mbork.pl/2021-05-02_Org-mode_to_Markdown_via_the_clipboard
+  (defun org-copy-region-as-markdown ()
+    "Copy the region (in Org) to the system clipboard as Markdown."
+    (interactive)
+    (if (use-region-p)
+	(let* ((region
+		(buffer-substring-no-properties
+		 (region-beginning)
+		 (region-end)))
+	       (markdown
+		(org-export-string-as region 'md t '(:with-toc nil))))
+	  (gui-set-selection 'CLIPBOARD markdown))))
+
+  :bind (("C-c l" . org-store-link)
+	 ("C-c a" . org-agenda)))
+
+(use-package org-tempo :ensure org)
+
+(use-package org-mac-link)
+
+(use-package ox-md :ensure org)
+
+(use-package org-roam
+  :after org
+  :init (setq org-roam-v2-ack t)
+  :custom (org-roam-directory jcs/org-roam-dir)
+  :bind (("C-c o l" . org-roam-buffer-toggle)
+	 ("C-c o f" . org-roam-node-find)
+	 ("C-c o g" . org-roam-graph)
+	 ("C-c o i" . org-roam-node-insert)
+	 ("C-c o c" . org-roam-capture)
+	 ("C-c o r" . org-roam-refile)
+	 ;; Dailies
+	 ("C-c o d" . org-roam-dailies-goto-today)
+	 ("C-c o p" . org-roam-dailies-goto-previous-note)
+	 ("C-c o n" . org-roam-dailies-goto-next-note)
+	 ("C-c o j" . org-roam-dailies-capture-today))
+  :hook ((find-file . vulpea-project-update-tag)
+	 (before-save . vulpea-project-update-tag))
+  :config
+  (org-roam-db-autosync-mode)
+  (advice-add 'org-roam-refile :after 'org-save-all-org-buffers)
+
+  ;; Help make agenda loading faster by only including org-roam files
+  ;; with todo headers in the agenda files
+  ;; Stolen from
+  ;; https://d12frosted.io/posts/2021-01-16-task-management-with-roam-vol5.html
+  (add-to-list 'org-tags-exclude-from-inheritance "project")
+
+  (defun vulpea-project-p ()
+    "Return non-nil if current buffer has any todo entry.
+
+TODO entries marked as done are ignored, meaning this function
+returns nil if current buffer contains only completed or
+canceled tasks."
+    (org-element-map
+	(org-element-parse-buffer 'headline)
+	'headline
+      (lambda (headline)
+	(eq (org-element-property :todo-type headline)
+	    'todo))
+      nil
+      'first-match))
+
+  ;; Update org-roam node tags with a special tag to help filter
+  ;; org-agenda buffers.
+
+  (defun vulpea-project-update-tag ()
+    "Update PROJECT tag in the current buffer."
+    (when (and (not (active-minibuffer-window))
+	       (org-roam-buffer-p))
+      (save-excursion
+	(goto-char (point-min))
+	(let* ((tags (vulpea-buffer-tags-get))
+	       (original-tags tags))
+	  (if (vulpea-project-p)
+	      (setq tags (cons "project" tags))
+	    (setq tags (remove "project" tags)))
+
+	  ;; Remove duplicates
+	  (setq tags (seq-uniq tags))
+
+	  ;; Update tags in the buffer if they've changed
+	  (when (or (seq-difference tags original-tags)
+		    (seq-difference original-tags tags))
+	    (apply #'vulpea-buffer-tags-set tags))))))
+
+  (defun org-roam-buffer-p ()
+    "Return non-nil of the currently visited buffer is an org-roam buffer."
+    (and buffer-file-name
+	 (string-prefix-p
+	  (expand-file-name (file-name-as-directory org-roam-directory))
+	  (file-name-as-directory buffer-file-name)))))
+
 (use-package restclient)
+(use-package ob-restclient)
 
 ;; Use es-mode for ElasticSearch buffers
 (use-package es-mode)
+(use-package ob-elasticsearch
+  :ensure es-mode
+  :config (add-to-list 'org-babel-load-languages '(elasticsearch . t)))
+
+;; org-babel
+(use-package ob
+  :ensure org
+  :config
+  (org-babel-do-load-languages 'org-babel-load-languages
+			       '((clojure . t)
+				 (shell . t)
+				 (sql . t)
+				 (emacs-lisp . t)
+				 (elasticsearch . t)
+				 (restclient . t))))
+
+(use-package org-agenda
+  :ensure org
+  :after org
+  :config
+  ;; TODO: Use a `let*` binding here and turn on lexical scoping for
+  ;; this file.
+  ;; Use the current window to open the agenda
+  (setq org-agenda-window-setup 'current-window
+	org-agenda-block-separator nil
+	org-agenda-tags-column -80
+	org-agenda-show-future-repeats nil)
+
+  ;; Stolen from
+  ;; https://d12frosted.io/posts/2020-06-24-task-management-with-roam-vol2.html
+  ;; This ensures that the label prior to the TODO in the agenda is
+  ;; readable and sane. This is especially useful using org-roam,
+  ;; since the filenames are prefixed with a timestamp, making the
+  ;; usual pattern (the filename) useless.
+  (setq org-agenda-prefix-format
+	'((agenda . " %i %(vulpea-agenda-category 12)%?-12t% s")
+	  (todo . " %i %(vulpea-agenda-category 12) ")
+	  (tags . " %i %(vulpea-agenda-category 12) ")
+	  (search . " %i %(vulpea-agenda-category 12) ")))
+
+  (defun vulpea-agenda-category (&optional len)
+    "Get category of item at point for agenda.
+
+     Category is defined by one of the following items:
+
+     - CATEGORY property
+     - TITLE keyword
+     - TITLE property
+     - filename without directory and extension
+
+     When LEN is a number, resulting string is padded right with
+     spaces and then truncated with ... on the right if result is
+     longer than LEN.
+
+     Usage example:
+
+       (setq org-agenda-prefix-format
+	     '((agenda . \" %(vulpea-agenda-category) %?-12t %12s\")))
+
+     Refer to `org-agenda-prefix-format' for more information."
+    (let* ((file-name (when buffer-file-name
+			(file-name-sans-extension
+			 (file-name-nondirectory buffer-file-name))))
+	   (title (vulpea-buffer-prop-get "title"))
+	   (category (org-get-category))
+	   (result
+	    (or (if (and
+		     title
+		     (string-equal category file-name))
+		    title
+		  category)
+		"")))
+      (if (numberp len)
+	  (s-truncate len (s-pad-right len " " result))
+	result)))
+
+  (defun vulpea-project-files ()
+    "Return a list of org-roam files containing the 'project' tag."
+    (seq-uniq
+     (seq-map
+      #'car
+      (org-roam-db-query
+       [:select [nodes:file]
+		:from tags
+		:left-join nodes
+		:on (= tags:node-id nodes:id)
+		:where (like tag (quote "%\"project\"%"))]))))
+
+  (defun vulpea-agenda-files-update (&rest _)
+    "Update the value of `org-agenda-files' based on 'project' tag."
+    (setq org-agenda-files (vulpea-project-files)))
+
+  (advice-add 'org-agenda :before #'vulpea-agenda-files-update)
+  (advice-add 'org-todo-list :before #'vulpea-agenda-files-update)
+
+  (defun jcs/tomorrow ()
+    "Returns a timestamp representing midnight of the next day."
+    (let ((current (decode-time (current-time))))
+      (setcar current 0)
+      (setcar (cdr current) 0)
+      (setcar (nthcdr 2 current) 0)
+      (setcar (nthcdr 3 current) (+ 1 (nth 3 current)))
+      (encode-time current)))
+
+  ;; Mostly borrowed from https://stackoverflow.com/a/54704297
+  (defun jcs/org-skip-function (part)
+    "Partitions things to decide if they should go into the agenda '(agenda future-scheduled done)"
+    (let* ((skip (save-excursion (org-entry-end-position)))
+           (dont-skip nil)
+           (scheduled-time (org-get-scheduled-time (point)))
+           (result
+            (or (and scheduled-time
+		     ;; This makes sure that tasks scheduled for a future date
+		     ;; (and not a future timestamp), which have a scheduled
+		     ;; time that's equivalent to midnight, are skipped unless
+		     ;; they're actually scheduled for today.
+                     (time-less-p (time-add (jcs/tomorrow) -1) scheduled-time)
+                     'future-scheduled)	; This is scheduled for a future date
+                (and (org-entry-is-done-p) ; This entry is done and should probably be ignored
+                     'done)
+                'agenda)))	       ; Everything else should go in the agenda
+      (if (eq result part) dont-skip skip)))
+
+  (setq org-agenda-custom-commands
+	'(("c" "Agenda and tasks"
+	   ((agenda ""
+		    ((org-agenda-skip-function
+		      '(org-agenda-skip-if nil '(todo done)))))
+	    (todo "BLOCKED"
+		  ((org-agenda-overriding-header "Blocked")
+		   (org-agenda-skip-function
+		    '(jcs/org-skip-function 'agenda))))
+	    (todo "DOING"
+		  ((org-agenda-overriding-header "In Progress")))
+	    (todo "TODO"
+		  ((org-agenda-overriding-header "Todo")
+		   (org-agenda-skip-function
+		    '(jcs/org-skip-function 'agenda))))
+	    (todo "WAITING"
+		  ((org-agenda-overriding-header "Waiting")
+		   (org-agenda-skip-function
+		    '(jcs/org-skip-function 'agenda))))
+	    (todo "DELEGATED"
+		  ((org-agenda-overriding-header "Delegated")
+		   (org-agenda-skip-function
+		     '(jcs/org-skip-function 'agenda))))
+	    (todo "HAMMOCK"
+		  ((org-agenda-overriding-header "Hammock")
+		   (org-agenda-skip-function
+		    '(jcs/org-skip-function 'agenda)))))))))
+
+(use-package orglink
+  :config (global-orglink-mode))
 
 (use-package autorevert
   :ensure f
@@ -399,7 +717,6 @@ Passes ARG onto `zap-to-char` or `backward-kill-word` if used."
 
 (use-package dockerfile-mode
   :mode "Dockerfile")
-
 (use-package yaml-mode
   :mode (("\\.yml\\'" . yaml-mode)
 	 ("\\.sls\\'" . yaml-mode)))
