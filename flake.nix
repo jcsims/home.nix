cc306a818f3f3005895f97e70feacf42c5cddc1e
Chris Sims <chris@jcsi.ms>
2023-01-18 20:56:04 -0800

    Pin to stable releases of nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hue = {
