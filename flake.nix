abde21552434da49662f8f5d5e37269828d787f1
Chris Sims <chris@jcsi.ms>
2023-02-23 12:22:04 -0800

    Remove the hue program, since I'm not using it currently.

--- flake.nix
+++ flake.nix
@@ -9,16 +9,11 @@
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    hue = {
-      url = "github:SierraSoftworks/hue?rev=4f597d972ab553208074ba19b9aaaa442fa8e43c";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = {
     nixpkgs,
     home-manager,
-    hue,
     unstable,
     ...
   }: let
@@ -45,7 +40,7 @@
             # https://github.com/babashka/process/commit/9e19562e108381be7bced275a9065dc182ec1c62
             babashka = unstable_pkgs.babashka;
           };
-          unfree-packages = [];
+          unfree-packages = ["plexamp"];
           username = "jcsims";
           homedir =
             if pkgs.stdenv.isDarwin
@@ -68,12 +63,11 @@
           inherit system;
           # Use this to pull in packages as flakes.
           extraPackages = {
-            hue = hue.packages.${system}.default;
             # Pull in a newer babashka so I can get > 1.0.168:
             # https://github.com/babashka/process/commit/9e19562e108381be7bced275a9065dc182ec1c62
             babashka = unstable_pkgs.babashka;
           };
-          unfree-packages = ["elasticsearch"];
+          unfree-packages = ["elasticsearch" "plexamp"];
           username = "chrsims";
           homedir =
             if pkgs.stdenv.isDarwin
