09e50bec83ef79f9c3cf32fe911e386b4b5b8e06
Chris Sims <chris@jcsi.ms>
2023-02-23 16:03:40 -0800

    Revert "Remove the hue program, since I'm not using it currently."
    This reverts commit abde21552434da49662f8f5d5e37269828d787f1.

--- flake.nix
+++ flake.nix
@@ -9,11 +9,16 @@
       url = "github:nix-community/home-manager/release-22.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    hue = {
+      url = "github:SierraSoftworks/hue?rev=4f597d972ab553208074ba19b9aaaa442fa8e43c";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     home-manager,
+    hue,
     unstable,
     ...
   }: let
@@ -40,7 +45,7 @@
             # https://github.com/babashka/process/commit/9e19562e108381be7bced275a9065dc182ec1c62
             babashka = unstable_pkgs.babashka;
           };
-          unfree-packages = ["plexamp"];
+          unfree-packages = [];
           username = "jcsims";
           homedir =
             if pkgs.stdenv.isDarwin
@@ -63,11 +68,12 @@
           inherit system;
           # Use this to pull in packages as flakes.
           extraPackages = {
+            hue = hue.packages.${system}.default;
             # Pull in a newer babashka so I can get > 1.0.168:
             # https://github.com/babashka/process/commit/9e19562e108381be7bced275a9065dc182ec1c62
             babashka = unstable_pkgs.babashka;
           };
-          unfree-packages = ["elasticsearch" "plexamp"];
+          unfree-packages = ["elasticsearch"];
           username = "chrsims";
           homedir =
             if pkgs.stdenv.isDarwin
