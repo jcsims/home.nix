1e993a48b953acb1c6ed443a6ad3e7ccb3f7ebc1
Chris Sims <chris@jcsi.ms>
2023-04-12 10:17:51 -0700

    Move to newer emacs package
    Also, use native-comp - it works with the newer package.

--- flake.nix
+++ flake.nix
@@ -83,6 +83,7 @@
               # Pull in a newer babashka so I can get > 1.0.168:
               # https://github.com/babashka/process/commit/9e19562e108381be7bced275a9065dc182ec1c62
               babashka = unstable_pkgs.babashka;
+              emacs = unstable_pkgs.emacs;
             };
             username = "chrsims";
             homedir = "/Users/${username}";
--- mac-gui.nix
+++ mac-gui.nix
@@ -1,9 +1,5 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # native comp just spews a ton of errors on mac currently.
-    (pkgs.emacs.override {
-      nativeComp = false;
-    })
     iterm2
     pinentry_mac
     terminal-notifier
