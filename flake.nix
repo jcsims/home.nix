cd48b6ba3750a6355a80412ee817027761f9f85b
Chris Sims <chris@jcsi.ms>
2023-08-10 07:07:02 -0700

    nix: pin flakes to stable versions, mostly

--- flake.nix
+++ flake.nix
@@ -3,9 +3,9 @@
 
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hue = {
