34cdbe2ddd0e93271ab0bca40c24a4d747d308d0
Chris Sims <chris@jcsi.ms>
2023-10-30 12:06:02 -0700

    nix: Clean up things that are brought in elsewhere

--- work.nix
+++ work.nix
@@ -11,29 +11,20 @@ rec {
   home.packages =
     (lib.attrValues specialArgs.extraPackages)
     ++ (with pkgs; [
-      apacheKafka
-      azure-cli
-      #babashka-unwrapped
-      babashka
-      clojure
       etcd_3_5
       google-cloud-sdk # `gcloud` CLI tool
-      go_1_19
       specialArgs.pkgs-unstable.gopls
       specialArgs.pkgs-unstable.graphite-cli
       grpcurl
       k9s
       kubeconform
-      kubectl
       kubectx
       kubelogin
-      kubernetes-helm
       kustomize
-      mysql
       pipx
       postgresql
       python3Packages.python-lsp-server
-      sops
+      nodejs
       teleport
       nodePackages.sql-formatter
       nodePackages.typescript-language-server
