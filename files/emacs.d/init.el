cbb574cf1cd34507f1b564c6618978c96d5113c8
Chris Sims <chris@jcsi.ms>
2023-12-15 22:17:35 -0800

    emacs: Add a yaml-path helper

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -1074,6 +1074,51 @@ format. With PREFIX, copy to kill ring."
 
     (global-set-key (kbd "C-c j") 'jcs/decode-jwt-at-point))
 
+  (progn ;; Borrowed from
+    ;; https://github.com/chopmo/dotfiles/blob/master/.emacs.d/customizations/jpt-yaml.el
+    ;; Print the yaml path at point.
+    (defun jpt-yaml-indentation-level (s)
+      (if (string-match "^ " s)
+          (+ 1 (jpt-yaml-indentation-level (substring s 1)))
+        0))
+
+    (defun jpt-yaml-current-line ()
+      (buffer-substring-no-properties (point-at-bol) (point-at-eol)))
+
+    (defun jpt-yaml-clean-string (s)
+      (let* ((s (replace-regexp-in-string "^[ -:]*" "" s))
+             (s (replace-regexp-in-string ":$" "" s)))
+        s))
+
+    (defun jpt-yaml-not-blank-p (s)
+      (string-match "[^[:blank:]]" s))
+
+    (defun jpt-yaml-path-to-point ()
+      (save-excursion
+        (let* ((line (jpt-yaml-current-line))
+               (level (jpt-yaml-indentation-level line))
+               result)
+          (while (> (point) (point-min))
+            (beginning-of-line 0)
+            (setq line (jpt-yaml-current-line))
+
+            (let ((new-level (jpt-yaml-indentation-level line)))
+              (when (and (jpt-yaml-not-blank-p line)
+                         (< new-level level))
+
+                (setq level new-level)
+                (setq result (push (jpt-yaml-clean-string line) result)))))
+
+          (mapconcat 'identity result " => "))))
+
+    (defun jpt-yaml-show-path-to-point ()
+      (interactive)
+      (message (jpt-yaml-path-to-point)))
+
+    (eval-after-load 'yaml-ts-mode
+      '(progn
+         (define-key yaml-ts-mode-map (kbd "C-c p") 'jpt-yaml-show-path-to-point))))
+
 
   (let ((file (expand-file-name (concat (user-real-login-name) ".el")
                                 user-emacs-directory)))
