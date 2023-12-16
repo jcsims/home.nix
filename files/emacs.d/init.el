100dd516776fe13458c879370fdade8506874426
Chris Sims <chris@jcsi.ms>
2023-12-15 22:21:32 -0800

    emacs: Use yaml-ts-mode over yaml-mode

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -935,6 +935,8 @@ Passes ARG onto `zap-to-char` or `backward-kill-word` if used."
   :ensure f
   :custom (xref-search-program 'ripgrep))
 
+(use-package yaml-ts-mode :ensure f)
+
 ;;; Tequila worms
 
 (progn ;     startup
