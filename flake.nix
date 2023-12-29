ff312c3b4574cb3185d13effa2293cb93aaee22b
Chris Sims <chris@jcsi.ms>
2023-12-29 11:20:43 -0800

    nix: Update stable to 23.11

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs-unstable.url = "github:NixOS/nixpkgs";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hue = {
