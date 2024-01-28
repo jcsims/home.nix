e510fa3e19346195fed160f5da3e11c96c51bc02
Chris Sims <chris@jcsi.ms>
2024-01-27 23:14:52 -0800

    Use stable's emacs-29 package

--- emacs.nix
+++ emacs.nix
@@ -1,7 +1,5 @@
-{
-  pkgs,
-  specialArgs,
-  ...
+{ pkgs
+, ...
 }: {
   home.packages = with pkgs; [
     (pkgs.emacsWithPackagesFromUsePackage {
@@ -25,7 +23,7 @@
       defaultInitFile = true;
 
       # Package is optional, defaults to pkgs.emacs
-      package = specialArgs.pkgs-unstable.emacs;
+      package = pkgs.emacs29;
 
       # By default emacsWithPackagesFromUsePackage will only pull in
       # packages with `:ensure`, `:ensure t` or `:ensure <package name>`.
