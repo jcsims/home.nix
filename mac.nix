0b7678ae6dde630fd9cdfb586c546b1f6d0e9ab8
Chris Sims <chris@jcsi.ms>
2024-03-10 09:06:50 -0700

    Fix macOS app linking

--- mac.nix
+++ mac.nix
@@ -37,17 +37,17 @@
   # Sync any applications installed managed via home-manager, so that Alfred
   # picks them up properly.
   home.activation = {
-    aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      for app in ~/.nix-profile/Applications/*
+    aliasApplications = lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" "installPackages" ] ''
+      for app in ~/Applications/Home\ Manager\ Apps/*
       do
-          app_name=''${app#~/.nix-profile/Applications/}
+          app_name=''${app#~/Applications/Home\ Manager\ Apps/}
           echo "Creating alias for: $app_name"
           ${pkgs.mkalias}/bin/mkalias -L "$app" ~/nix-apps/"$app_name"
       done
 
       for app in ~/nix-apps/*
       do
-          if ! [[ -e ~/.nix-profile/Applications/"''${app#~/nix-apps/}" ]]
+          if ! [[ -e ~/Applications/Home\ Manager\ Apps/"''${app#~/nix-apps/}" ]]
           then
               echo "$app is getting removed"
               rm -- "$app"
