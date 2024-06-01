c1f5a1493227c3847907d42a1b3f16a0d71bec07
Chris Sims <chris@jcsi.ms>
2024-05-31 18:57:08 -0700

    nix: Update to 24.05

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     hue = {
