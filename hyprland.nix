c6adc4c6fa432921e3ead40f864606313bd73255
Chris Sims <chris@jcsi.ms>
2024-05-31 22:51:45 -0700

    nix: fix hypridle settings

--- hyprland.nix
+++ hyprland.nix
@@ -201,23 +201,25 @@
 
   services.hypridle = {
     enable = true;
-    general = {
-      after_sleep_cmd = "hyprctl dispatch dpms on";
-      ignore_dbus_inhibit = false;
-      lock_cmd = "hyprlock";
-    };
+    settings = {
+      general = {
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+        lock_cmd = "hyprlock";
+      };
 
-    listener = [
-      {
-        timeout = 900;
-        on-timeout = "hyprlock";
-      }
-      {
-        timeout = 1200;
-        on-timeout = "hyprctl dispatch dpms off";
-        on-resume = "hyprctl dispatch dpms on";
-      }
-    ];
+      listener = [
+        {
+          timeout = 900;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 1200;
+          on-timeout = "hyprctl dispatch dpms off";
+          on-resume = "hyprctl dispatch dpms on";
+        }
+      ];
+    };
   };
 
   programs.hyprlock.enable = true;
