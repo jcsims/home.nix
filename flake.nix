cd771298258b5d2f3867e485e7a7036dc3721008
Chris Sims <jcsims@groot.discus-herring.ts.net>
2024-07-10 19:16:33 -0700

    Use the right nixpkgs stable pin

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
