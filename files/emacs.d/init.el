6ba25ef46ca99f705f800952f34ff4967650e952
Chris Sims <chris@jcsi.ms>
2024-07-23 10:24:58 -0700

    emacs: Add more work bug references

--- files/emacs.d/init.el
+++ files/emacs.d/init.el
@@ -124,7 +124,7 @@
 (use-package bug-reference
   :ensure f
   :config
-  (setq bug-reference-bug-regexp "\\(\\(RATE-[0-9]+\\)\\)"
+  (setq bug-reference-bug-regexp "\\(\\(RATE-[0-9]+\\|AR-[0-9]+\\)\\)"
         bug-reference-url-format "https://splashfinancial.atlassian.net/browse/%s")
   :hook (prog-mode . bug-reference-prog-mode))
 
